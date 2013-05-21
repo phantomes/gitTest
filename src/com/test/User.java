d679371ecf2feac892c7d69caa8c56bc7e595414
phantomes <bluesroot@gmail.com>
2013-05-21 21:39:10 +0800

    添加user类

---
+++ src/com/test/User.java
@@ -0,0 +1,27 @@
+package com.test;
+
+public class User {
+	private Long id;
+	private String name;
+	private String ageS;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAgeS() {
+		return ageS;
+	}
+	public void setAgeS(String ageS) {
+		this.ageS = ageS;
+	}
+	
+	
+}
